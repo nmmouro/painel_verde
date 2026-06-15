c81fd2ed5aac9e0b29c02da7aca5b55e64757306
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 17:13:53 -0300

    Add renderTable function to generate HTML table

--- js/render.js
+++ js/render.js
@@ -1,1 +1,36 @@
+function renderTable(headers, rows){
 
+  return `
+  <table>
+    <thead>
+      <tr>${headers.map(h=>`<th>${h}</th>`).join("")}</tr>
+    </thead>
+    <tbody>
+
+      ${rows.map(row=>{
+
+        const v = (row.c||[]).map(getCell);
+        const status = v.at(-1);
+
+        return `
+        <tr class="${getStatusClass(status)}">
+
+          ${v.map((cell,i)=>{
+            const last = i === v.length - 1;
+
+            return `
+              <td class="${last ? "status-icon" : ""}">
+                ${last ? getStatusIcon(status) : cell}
+              </td>
+            `;
+          }).join("")}
+
+        </tr>
+        `;
+
+      }).join("")}
+
+    </tbody>
+  </table>
+  `;
+}
