610357623f2c2afda5df1308c2f6af6e419db16e
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 17:38:29 -0300

    Update 01config.js

--- js/01config.js
+++ js/01config.js
@@ -1,46 +1,9 @@
-const SHEET_ID =
-  "14T7zF0likp8YGeycOYBcOs9INY9gvMzpt7SMfghE8P4";
+const SHEET_ID = "14T7zF0likp8YGeycOYBcOs9INY9gvMzpt7SMfghE8P4";
 
 const TABS = [
-
-  {
-    id:"veiculos",
-    title:"VEÍCULOS",
-    sheet:"VEÍCULOS",
-
-    
-  },
-
-  {
-  id:"motoristas",
-  title:"MOTORISTAS",
-  sheet:"MOTORISTAS",
-
-  
-},
-
-  {
-    id:"painel",
-    title:"PAINEL",
-    sheet:"LANÇAMENTOS",
-
-   
-  },
-
-  {
-    id:"agenda",
-    title:"AGENDA DO DIA",
-    sheet:"AGENDA DO DIA",
-
-    
-  },
-
-  {
-    id:"social",
-    title:"AGENDA SERVIÇO SOCIAL",
-    sheet:"AGENDA SERVIÇO SOCIAL",
-
-   
-  }
-
+  "VEÍCULOS",
+  "MOTORISTAS",
+  "LANÇAMENTOS",
+  "AGENDA DO DIA",
+  "AGENDA SERVIÇO SOCIAL"
 ];
