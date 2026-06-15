0773bc5b583facd126f540097ad08099dbbe8c14
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 16:37:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,9 @@
-export const CONFIG = {
-  SHEET_ID: "14T7zF0likp8YGeycOYBcOs9INY9gvMzpt7SMfghE8P4",
+const SHEET_ID = "14T7zF0likp8YGeycOYBcOs9INY9gvMzpt7SMfghE8P4";
 
-  TABS: [
-    {
-      id: "veiculos",
-      title: "VEÍCULOS",
-      sheet: "VEÍCULOS",
-      query: "select B,V,M,N"
-    },
-    {
-      id: "motoristas",
-      title: "MOTORISTAS",
-      sheet: "MOTORISTAS",
-      query: "select A,B,C"
-    },
-    {
-      id: "painel",
-      title: "PAINEL",
-      sheet: "LANÇAMENTOS",
-      query: "select B,C,D,E,F,G,X"
-    },
-    {
-      id: "agenda",
-      title: "AGENDA DO DIA",
-      sheet: "AGENDA DO DIA",
-      query: "select B,C,D,E,F,G,H"
-    },
-    {
-      id: "social",
-      title: "SERVIÇO SOCIAL",
-      sheet: "AGENDA SERVIÇO SOCIAL",
-      query: "select B,C,D,F,G,H"
-    }
-  ]
-};
+const TABS = [
+  "VEÍCULOS",
+  "MOTORISTAS",
+  "LANÇAMENTOS",
+  "AGENDA DO DIA",
+  "AGENDA SERVIÇO SOCIAL"
+];
