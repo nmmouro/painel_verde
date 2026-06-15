866b70f62e4a2aece4a322c82ca4c826bd2c9fb0
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 16:38:43 -0300

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,13 +1,19 @@
-export const getCell = c => c?.f ?? c?.v ?? "";
+const parseGviz = txt =>
+  JSON.parse(txt.substring(txt.indexOf("{"), txt.lastIndexOf("}") + 1));
 
-export function normalize(text = "") {
-  return text
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/\p{Diacritic}/gu, "");
-}
+const getCell = c => c?.f ?? c?.v ?? "";
 
-export function formatDateBR(date) {
-  if (!date) return "";
-  return new Date(date).toLocaleDateString("pt-BR");
+function parseDataHoraBR(data, hora="00:00"){
+  if(!data) return new Date(0);
+
+  const [dia, mes, ano] = String(data).split("/");
+  const [h, m] = String(hora).split(":");
+
+  return new Date(
+    Number(ano),
+    Number(mes) - 1,
+    Number(dia),
+    Number(h||0),
+    Number(m||0)
+  );
 }
