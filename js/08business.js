2a7042a4916b22f979c5aea097a513360c5d40e7
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-15 17:49:21 -0300

    Update 08business.js

--- js/08business.js
+++ js/08business.js
@@ -1,1 +1,24 @@
+function getPainelRows(results){
 
+  const painelIndex =
+    TABS.findIndex(t => t === "LANÇAMENTOS");
+
+  return (results[painelIndex].table.rows || [])
+    .filter(row => {
+
+      const v =
+        (row.c || []).map(getCell);
+
+      const status =
+        String(v[6] || "")
+          .toUpperCase()
+          .trim();
+
+      return (
+        status.includes("AGENDADO") ||
+        status.includes("EM ANDAMENTO") ||
+        status.includes("VIAGEM") ||
+        status.includes("MANUTENCAO")
+      );
+    });
+}
