9667ea179567f82b183422df44ef848b429b7eb1
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-16 08:32:23 -0300

    Update 01config.js

--- js/01config.js
+++ js/01config.js
@@ -1,9 +1,78 @@
 const SHEET_ID = "14T7zF0likp8YGeycOYBcOs9INY9gvMzpt7SMfghE8P4";
 
 const TABS = [
-  "VEÍCULOS",
-  "MOTORISTAS",
-  "LANÇAMENTOS",
-  "AGENDA DO DIA",
-  "AGENDA SERVIÇO SOCIAL"
+  
+  {
+    id:"veiculos",
+    title:"VEÍCULOS",
+    sheet:"VEÍCULOS",
+
+    headers:[
+      "TRAIL TXN1I89",
+      "CRETA TXJ6F18",
+      "CRETA TXJ6F19",
+      "CRETA TXJ6F20"
+    ]
+  },
+
+  {
+  id:"motoristas",
+  title:"MOTORISTAS",
+  sheet:"MOTORISTAS",
+
+  headers:[
+    "Motorista",
+    "Status",
+    "Condição",
+    
+  ]
+},
+
+  {
+    id:"painel",
+    title:"PAINEL",
+    sheet:"LANÇAMENTOS",
+
+    headers:[
+      "Data",
+      "Hora",
+      "Empregado",
+      "Veículo",      
+      "Passageiro/ Setor/ Motivo",
+      "Itinerário",
+      "Status"
+    ]
+  },
+
+  {
+    id:"agenda",
+    title:"AGENDA DO DIA",
+    sheet:"AGENDA DO DIA",
+
+    headers:[
+      "Data",
+      "Hora",
+      "Passageiro",
+      "Setor",
+      "Motivo",
+      "Itinerário",
+      "Status"
+    ]
+  },
+
+  {
+    id:"social",
+    title:"AGENDA SERVIÇO SOCIAL",
+    sheet:"AGENDA SERVIÇO SOCIAL",
+
+    headers:[
+      "Data",
+      "Período",
+      "Passageiro",
+      "Motivo",
+      "Itinerário",
+      "Status"
+    ]
+  }
+
 ];
