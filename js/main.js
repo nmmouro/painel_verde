8d397a263493154d00eb51dc287d90d1dd1480e9
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 14:50:58 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import { getData } from "../app/api.js";
+import { store } from "../app/store.js";
+import { renderList } from "../app/ui.js";
+
+import "./clock.js";
+import "./fullscreen.js";
+
+async function load() {
+  store.painel = await getData("painel");
+  store.agenda = await getData("agenda");
+  store.social = await getData("social");
+
+  render();
+}
+
+function render() {
+  renderList(document.getElementById("painelData"), store.painel);
+  renderList(document.getElementById("agendaData"), store.agenda);
+  renderList(document.getElementById("socialData"), store.social);
+}
+
+load();
