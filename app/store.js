401ef340de4980d787160f8bfb5bb20eb4138aa0
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 14:43:12 -0300

    Create store.js with initial data structure
    Initialized store with painel, agenda, social, veículos, and motoristas sections.

---
+++ app/store.js
@@ -0,0 +1,47 @@
+export const store = {
+  painel: [
+
+    {
+    id:"painel",
+    title:"PAINEL",
+    sheet:"LANÇAMENTOS"
+    }
+  ],
+
+  
+  agenda: [
+     {
+    id:"agenda",
+    title:"AGENDA DO DIA",
+    sheet:"AGENDA DO DIA"
+     }
+  ],
+
+  
+  social: [
+     {
+    id:"social",
+    title:"AGENDA SERVIÇO SOCIAL",
+    sheet:"AGENDA SERVIÇO SOCIAL"
+     }
+  ],
+
+  
+   veículos:[
+    {
+    id:"veiculos",
+    title:"VEÍCULOS",
+    sheet:"VEÍCULOS"
+    }
+  ],
+
+
+    motoristas:[
+    {
+    id:"motoristas",
+    title:"MOTORISTAS",
+    sheet:"MOTORISTAS"
+    }
+   ]
+    
+    };
