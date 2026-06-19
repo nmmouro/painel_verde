1702e26308a285c23d986b6743a141bf8af3c799
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 22:11:24 -0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -13,24 +13,6 @@ setInterval(() => {
 }, 1000);
 
 /*
-
-
-function updateClock() {
-  const now = new Date();
-
-  document.getElementById("clock").innerText =
-    now.toLocaleDateString("pt-BR") +
-    " — " +
-    now.toLocaleTimeString("pt-BR", {
-      hour: "2-digit",
-      minute: "2-digit"
-    });
-}
-
-updateClock();              // executa imediatamente
-setInterval(updateClock, 1000); // atualiza a cada segundo
- 
-
 setInterval(() => {
   const now = new Date().toLocaleTimeString();
   document.getElementById("clock").innerText = now;
