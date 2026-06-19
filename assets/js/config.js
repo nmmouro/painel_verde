4140c83948bbb589021e9b99c2f4c10729153e80
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-19 19:24:35 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycbwiaVBLh1B4ZpMbyDNX5RIhFhhUxX9jni8M6vmtFoYRaU2-7eazccWwngHzjajXp44t8w/exec";
-
-export const TABS = [
-  { id:"veiculos", sheet:"VEÍCULOS" },
-  { id:"motoristas", sheet:"MOTORISTAS" },
-  { id:"painel", sheet:"LANÇAMENTOS" },
-  { id:"agenda", sheet:"AGENDA DO DIA" },
-  { id:"social", sheet:"AGENDA SERVIÇO SOCIAL" }
-];
