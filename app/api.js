d155d247cb9f5232c313b99f5845f2ad21055368
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 21:32:33 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -1,25 +1,5 @@
-/*
+
 export async function getData(tipo) {
   const res = await fetch(`${API_URL}?tipo=${tipo}`);
   return await res.json();
 }
-*/
-
-
-
-  export function updateClock() {
-    const now = new Date();
-
-    document.getElementById("clock").innerText =
-      now.toLocaleDateString("pt-BR") +
-      " — " +
-      now.toLocaleTimeString("pt-BR", {
-        hour: "2-digit",
-        minute: "2-digit"
-      });
-  }
-
-  setInterval(updateClock, 1000);
-  updateClock();
-
-
