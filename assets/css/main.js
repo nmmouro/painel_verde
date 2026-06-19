a6288f335fae9b2fa0e9a67db31d3ef3e965358e
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-19 13:20:08 -0300

    Add agenda loading and rendering functionality

--- assets/css/main.js
+++ assets/css/main.js
@@ -1,1 +1,30 @@
 
+import { obterAgendaHoje} from ".js/agenda.js";
+
+const agendaRows =
+  await obterAgendaHoje(SHEET_ID);
+
+
+
+import { getData } from "../app/api.js";
+import { store } from "../app/store.js";
+import { renderList } from "../app/ui.js";
+
+import "./clock.js";
+import "./fullscreen.js";
+
+async function load() {
+  store.painel = await getData("painel");
+  store.agenda = await getData("agenda");
+  store.social = await getData("social");
+
+  render();
+}
+
+function render() {
+  renderList(document.getElementById("painelData"), store.painel);
+  renderList(document.getElementById("agendaData"), store.agenda);
+  renderList(document.getElementById("socialData"), store.social);
+}
+
+load();
