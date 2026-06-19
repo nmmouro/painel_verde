82a82ca5486fc67ee17fd409e2a41230203a49f5
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 21:04:10 -0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,27 @@
+  function updateClock() {
+    const now = new Date();
+
+    document.getElementById("clock").innerText =
+      now.toLocaleDateString("pt-BR") +
+      " — " +
+      now.toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit"
+      });
+  }
+
+  setInterval(updateClock, 1000);
+  updateClock();
+
+
+
+
+
+/*
+
+
 setInterval(() => {
   const now = new Date().toLocaleTimeString();
   document.getElementById("clock").innerText = now;
 }, 1000);
+*/
