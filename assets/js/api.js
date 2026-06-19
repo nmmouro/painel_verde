02710520c988eccac3cb3f30874161c7d43b2e19
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-19 13:05:32 -0300

    Create api.js

---
+++ assets/js/api.js
@@ -0,0 +1,17 @@
+import { API_URL } from "./config.js";
+
+export async function getSheet(sheet) {
+  try {
+    const res = await fetch(`${API_URL}?sheet=${encodeURIComponent(sheet)}`);
+
+    if (!res.ok) throw new Error("Erro API");
+
+    const json = await res.json();
+
+    return json.data || [];
+
+  } catch (err) {
+    console.error("Erro API:", err);
+    return [];
+  }
+}
