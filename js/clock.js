18f5f8464663b26314825a8e9e6014dfef9dce33
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 21:48:18 -0300

    Enhance clock.js to display date and time
    Update clock display to show date and time in Portuguese format.

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,11 @@
 setInterval(() => {
   const now = new Date().toLocaleTimeString();
-  document.getElementById("clock").innerText = now;
+  document.getElementById("clock").innerText =
+    now.toLocaleDateString("pt-BR") +
+    " — " +
+    now.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit";
 }, 1000);
 
 
