5bbe711e814ed698e96f625045b79ac7519ecec6
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-19 19:24:45 -0300

    Delete app directory

--- app/config/constants.js
+++
@@ -1,5 +0,0 @@
-export const SHEET_ID =
-"14T7zF0likp8YGeycOYBcOs9INY9gvMzpt7SMfghE8P4";
-
-export const WEBAPP =
-"https://script.google.com/macros/s/AKfycbwiaVBLh1B4ZpMbyDNX5RIhFhhUxX9jni8M6vmtFoYRaU2-7eazccWwngHzjajXp44t8w/exec";
--- app/ui.js
+++
@@ -1,8 +0,0 @@
-export function renderList(el, data) {
-  el.innerHTML = data.map(item => `
-    <div class="row">
-      <strong>${Object.values(item)[0]}</strong><br>
-      <span>${Object.values(item)[1]}</span>
-    </div>
-  `).join("");
-}
