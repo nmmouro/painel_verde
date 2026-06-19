aee93b168ab072e61573327dcfaa70eb6bbb2ae3
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-18 21:37:03 -0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,13 @@
 setInterval(() => {
- setInterval(() => {
-  const now = new Date().toLocaleTimeString();
-  document.getElementById("clock").innerText = now;
-}, 1000);
+  const now = new Date();
 
+  document.getElementById("clock").innerText =
+    now.toLocaleDateString("pt-BR") +
+    " — " +
+    now.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit"
+    }, 1000);
 
 
 
